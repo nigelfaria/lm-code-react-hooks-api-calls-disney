deb803e4a22d291aab3baa22746e23af2b4713d5
Nigel Faria <nigel.faria@gmail.com>
2023-02-01 19:57:27 +0000

    Activity 3 Adding new and previous page functionality

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [characters, setCharacters] = useState<Array<DisneyCharacter>>([])
   useEffect(() => {
-    getCharacters(1);
-  }, []);
+    getCharacters(currentPage);
+  }, [currentPage]);
 
   const getCharacters = async (pageNumber: number) => {
     const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
